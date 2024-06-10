757edbcfbf710a20d409da80269ab14f2387de36
MatheusThiesen <matheusreis314@gmail.com>
2024-06-10 20:05:48 -0300

    feat: add maps

--- App.tsx
+++ App.tsx
@@ -1,11 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
 
 export default function App() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0.02,
+    longitudeDelta: 0.02,
+  });
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (location) {
+      setMapRegion((data) => ({
+        ...data,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      }));
+    }
+  }, [location]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {mapRegion.latitude !== 0 && (
+        <MapView
+          style={styles.map}
+          showsUserLocation
+          region={mapRegion}
+        ></MapView>
+      )}
     </View>
   );
 }
@@ -13,8 +55,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
